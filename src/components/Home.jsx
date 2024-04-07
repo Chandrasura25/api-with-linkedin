a3e327e8d63371d7e862689c5dd918d7cbbdee23
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-04-07 13:49:58 +0100

    clerk

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
+import { SignInButton } from '@clerk/clerk-react';
+
 export default function Home(){
 	return(
 		<>
-	      <div className='h-screen w-full flex justify-center items-center'>
-	        <button className='px-4 py-2.5 border rounded'>Sign with LinkedIn</button>
-	      </div>
-    	</>
-	)
+			<SignInButton mode="modal" redirectUrl='/form'/>
+		</>
+		)
 }
