8f7e1edebeebbbae82b5edc677dc18e1a7736871
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-04-07 11:02:16 +0100

    linkedin

---
+++ src/components/Form.jsx
@@ -0,0 +1,9 @@
+export default function Form(){
+	return(
+		<>
+	      <div className='h-screen w-full flex justify-center items-center'>
+	        <button className='px-4 py-2.5 border rounded'>Sign with LinkedIn</button>
+	      </div>
+    	</>
+	)
+}
