f5250faca429900fc9302e2f3dbbbe67a7c8580e
Chandrasura25 <abdulganiy1624demilade@gmail.com>
2024-04-07 15:07:30 +0100

    clerk

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
 import { Routes, Route, useNavigate } from 'react-router-dom';
-import Home from './components/Home'
 import Form from './components/Form';
 import { SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react';
 function App() {
   return (
     <div className='h-screen flex justify-center items-center'>
    <Routes>
-     <Route path='/' element={<Home/>} />
-     <Route path='/form' element={
-      <>
-        <SignedIn>
-          <Form/>
-        </SignedIn>
-        <SignedOut>
-          <RedirectToSignIn/>
-        </SignedOut>
-      </>
-     } />
+     <Route path='/' element={<Form/>} />
    </Routes>
    </div>
   )
